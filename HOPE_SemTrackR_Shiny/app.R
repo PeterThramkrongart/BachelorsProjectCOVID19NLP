b47f09ae8e4687bc62bba15fe1e86ab8330c0ae4
Peter Thramkrongart <Peter.Thramkrongart@gmail.com>
2020-12-17 11:22:11 +0100

    Added GloVe text

--- HOPE_SemTrackR_Shiny/app.R
+++ HOPE_SemTrackR_Shiny/app.R
@@ -24,14 +24,16 @@ ui <- fluidPage(
             "Front Page",
             h2("Hope SemTrackR"),
             "Welcome to the HOPE SemTrackR.
-            This is a tool for exploring how the COVID-19 pandemic has the language used in Danish newspapers.
-            The models are based on the text from 1898 front pages from Danish mainstream newspapers.
-            This includes front pages from Berlingske Tidende, B.T., Ekstrabladet, Information, Jyllands Posten, Politiken, Kristeligt Dagblad, and Weekendavisen.
-            The data is gathered from 2019-12-02 to 2020-09-10. The data is divided into 3 distinct periods.
-            (1) A baseline period from before COVID-19 reached Denmark (before 2020-02-26),
-            (2) an outbreak period (2020-02-28 to 2020-04-12) spanning from the first infection cases in Denmark to the reopening of the first lockdown in Denmark,
-            and (3) a post outbreak period (after 2020-04-12) that covers a period after the lockdown.
-            Click on the tabs in the top panel to proceed to various models and start exploring!"
+ This is a tool for exploring how the COVID-19 pandemic has the language used in Danish newspapers.
+ The models are based on the text from 1898 front pages from Danish mainstream newspapers.
+ This includes front pages from Berlingske Tidende, B.T., Ekstrabladet, Information, Jyllands Posten, Politiken, Kristeligt Dagblad, and Weekendavisen.
+ The data was gathered from 2019-12-02 to 2020-09-10.
+ The data is divided into 3 distinct periods.
+ (1) A baseline period from before COVID-19 reached Denmark (before 2020-02-26),
+ (2) an outbreak period (2020-02-28 to 2020-04-12) spanning from the first infection cases in Denmark to the reopening of the first lockdown in Denmark,
+ and (3) a post outbreak period (after 2020-04-12) that covers a period after the lockdown.
+ Click on the tabs in the top panel to proceed to various models and start exploring!
+"
         )
         ,
         tabPanel(
@@ -55,13 +57,34 @@ ui <- fluidPage(
         ,
         tabPanel(
             "Word Similarity Search Engine",
-            h2("Word Association Search Engine"),
-            substr(lorem, 1, 500),
+            h2("Word Similarity Search Engine"),
+            "This is the SemTrackR Similarity Search Engine.
+ It searches for the top 20 words most similar to the search word in each period.
+ You can use the search word selector to select a word to search for.
+ The words are tagged with their word class. 
+ The classes are nouns, proper nouns, verbs, adjectives, and adverbs with the tags noun, propn,  verb, adj, and adv respectively.
+ The words have been transformed into their dictionary form (lemmatized) and transform to all lowercase. 
+ When searching, the search engine will attempt to find the words that are most similar to the search word.
+ If the search word does not appear more than five times in a period, the search cannot be computed for that specific period. 
+ \n
+How do we measure word similarity?
+ The meanings of the words are computationally inferred by how often words appear together.
+ The argument is that words that tend to appear in the same context must have a similar meaning.
+ These computational inferences can be represented as arrays of numbers called embeddings.
+ The mathematical model used for the embeddings used for this search engine is called GloVe-embeddings.
+ Each period has its own matrix of vocabulary represented in an embedded matrix.
+ The actual search consists of linear algebraic computation called cosine similarity, which calculates the similarity between the search word and each word in a matrix. \n
+DISCLAIMER:
+ GloVe-embeddings need a large amount of data to work properly.
+ The embeddings used for this search engine are therefore not trained on enough data to give robust results.
+ Use this search engine at your own discretion.\n
+Some ideas for search words are: sygdom_noun, økonomi_noun, smitte_verb, isolation_noun, and isolere_verb, test_noun
+",
             br(),
             br(),
             selectizeInput(
                 "GloVeLemma",
-                "Search word",
+                "Select Search Word",
                 totalVocab$term,
                 selected = "danmark_propn",
                 options = list(allowEmptyOption = FALSE)
